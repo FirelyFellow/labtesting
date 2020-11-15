917e4e92aacb844c85b0b5160c48b29b2417ce1e
SebastienColl <scoll010@uottawa.ca>
2020-11-14 23:32:14 -0500

    Added MainActivityTest2 and MainActivityTest3

--- app/src/androidTest/java/com/example/seg2105/labtesting/MainActivityTest3.java
+++ app/src/androidTest/java/com/example/seg2105/labtesting/MainActivityTest3.java
@@ -14,17 +14,15 @@ import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
-public class MainActivityTest2 {
+public class MainActivityTest3 {
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
     public void emailIsInvalid() {
-        onView(withId(R.id.username)).perform(typeText("user"), closeSoftKeyboard());
-        onView(withId(R.id.Lastname)).perform(typeText("test"), closeSoftKeyboard());
-        onView(withId(R.id.email)).perform(typeText("email@"), closeSoftKeyboard());
+        onView(withId(R.id.username)).perform(typeText("user1"), closeSoftKeyboard());
         onView(withId(R.id.loginBtn)).perform(click());
-        onView(withText("Invalid Email")).check(matches(isDisplayed()));
+        onView(withText("Invalid First Name")).check(matches(isDisplayed()));
     }
 }
